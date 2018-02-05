9e7eb9a0b2dde096cd8e655135d40fd38d08987b
setty48 <35407490+setty48@users.noreply.github.com>
2018-02-05 16:02:14 -0600

    Add files via upload
    This file shows how to use import and some builtin functions like ABS, MAX, MIN..etc

---
+++ 3import_builtinfunctions.py
@@ -0,0 +1,18 @@
+"""Page 3"""
+
+# 1.Import functions
+
+import random
+from random import randint
+from math import *
+
+print(randint(5, 10))      #random integer is picked up 
+print(factorial(4))        # Factorial of given value
+print(random.random())     # random float value between 0 and1 is selected randomly by processor
+
+# 2.BuiltIn Functions
+print(abs(-1.2))           # removes negative sign prints only positive values
+print(type(1.345))         # data type of value given is printed
+print(max(1, 9, 2, 3, 20)) # maximum value out of all given list is printed
+print(min("bca","cde","fgh")) # minimum value out of all given list is printed
+
